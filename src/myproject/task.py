0d18d09a008d4936336ee66e69c976a9d016af0f
zhamileka <zhamileka@gmail.com>
2024-11-10 21:17:54 +0000

    add mult f

--- src/myproject/task.py
+++ src/myproject/task.py
@@ -1,3 +1,7 @@
 # this is my project
 def add_nums(a, b):
     return a + b
+
+
+def mult_nums(a, b):
+    return a * b
--- tests/test_task.py
+++ tests/test_task.py
@@ -1,9 +1,15 @@
-from src.myproject.task import add_nums
+from src.myproject.task import add_nums, mult_nums
 
 
 def test_add_nums():
     assert add_nums(1, 2) == 3
 
 
+def test_mult_nums():
+    assert mult_nums(1, 20) == 20
+    assert mult_nums(2, 2) == 4
+
+
 if __name__ == "__main__":
     test_add_nums()
+    test_mult_nums()
